96f237db544961c8e937d5fe4e745c16e1ea3b54
Matheus Betim <mbetim47@gmail.com>
2022-05-12 21:00:41 -0300

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,21 @@
-$global:DefaultUser = [System.Environment]::UserName
-# Import-Module posh-git
-Import-Module oh-my-posh
+$global:DefaultUser = "Betim"
+oh-my-posh init pwsh --config "$(scoop prefix oh-my-posh)\themes\custom.omp.json" | Invoke-Expression 
+
 Import-Module -Name Terminal-Icons
-Set-PoshPrompt -Theme paradox
 
 # PSReadLine
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineKeyHandler -Chord "Ctrl+k" -Function ForwardChar
+Set-PSReadLineKeyHandler -Chord "Ctrl+j" -Function NextHistory
+Set-PSReadLineKeyHandler -Chord "Ctrl+k" -Function PreviousHistory
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias np C:\Windows\notepad.exe
+Set-Alias g git
+
+function gitStatus { git status}
+Set-Alias -Name gst -Value gitStatus
+
+function gitPull { git pull }
+Set-Alias -Name gpl -Value gitPull

7db340e251e2ada8e8cd10d7368277146029b792
Matheus Betim <mbetim47@gmail.com>
2023-05-11 22:10:50 -0300

    feat(zsh): set vim as the default editor

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -75,17 +75,9 @@ source $ZSH/oh-my-zsh.sh
 
 # User configuration
 
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
+# Set vim as default editor
+export VISUAL=vim
+export EDITOR="$VISUAL"
 
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"

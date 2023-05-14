5ff9fdc45160dad534c883ac42251f04b2cd7c7d
Matheus Betim <mbetim47@gmail.com>
2023-05-14 19:07:18 -0300

    feat(vim): update preferences for neo-tree

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				hide_by_name = {
+					"node_modules",
+					".git",
+				},
+			},
+		},
+	},
+}

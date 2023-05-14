12f50e4385a7c18008e3189e0c43283e21e24140
Matheus Betim <mbetim47@gmail.com>
2023-05-14 19:07:02 -0300

    feat(vim): remove keymaps for 's'

---
+++ nvim/lua/plugins/leap.lua
@@ -0,0 +1,18 @@
+-- This is being used to disable the keymaps to 's'
+return {
+	"ggandor/leap.nvim",
+	config = function()
+		return {
+			keys = {},
+			config = function(_, opts)
+				local leap = require("leap")
+				for k, v in pairs(opts) do
+					leap.opts[k] = v
+				end
+				leap.add_default_mappings(true)
+				vim.keymap.del({ "x", "o" }, "x")
+				vim.keymap.del({ "x", "o" }, "X")
+			end,
+		}
+	end,
+}

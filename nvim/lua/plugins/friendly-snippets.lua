b082fa3f6d5375944c1893120b40376bbac8471e
Matheus Betim <mbetim47@gmail.com>
2023-12-15 23:59:44 -0300

    fix(vim): use the `opts` to set the path for the snippets instead of `config`

--- nvim/lua/plugins/friendly-snippets.lua
+++ nvim/lua/plugins/friendly-snippets.lua
@@ -1,7 +1,6 @@
 return {
 	"rafamadriz/friendly-snippets",
-	config = function()
-		require("luasnip.loaders.from_vscode").lazy_load()
-		require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/snippets" } })
-	end,
+	opts = {
+		require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/snippets" } }),
+	},
 }

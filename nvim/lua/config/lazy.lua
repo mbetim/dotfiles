09fd836ca9e8eb5384a3888ea85f1abed506561b
Matheus Betim <mbetim47@gmail.com>
2023-12-15 23:30:09 -0300

    feat(vim): configure the dap to debug

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,11 +17,11 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
-
+		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
-		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.coding.yanky" },
+		{ import = "lazyvim.plugins.extras.dap.core" },
 
 		-- import/override with your plugins
 		{ import = "plugins" },

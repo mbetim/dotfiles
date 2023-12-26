f8fd179409b343862e05449e8d97c7867bf636ff
Matheus Betim <mbetim47@gmail.com>
2023-12-26 16:38:47 -0300

    feat(vim): always use the lts version when openning neovim
    It can have some changes later on to get the current user home dir and node lts version automatically, but it'll work for now

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- TODO: try to make this use the $HOME path and fetch the latest lts version automatically
+local node_bin = "/Users/matheus.betim/.nvm/versions/node/v20.10.0/bin"
+vim.g.node_host_prog = node_bin .. "/node"
+vim.cmd("let $PATH = '" .. node_bin .. ":' . $PATH")
+
 vim.opt.clipboard = nil
 
 vim.opt.spelllang = "en_us"

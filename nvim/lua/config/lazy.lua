1d9f5a0b8e0568b935e96899955b66f981f9581b
Matheus Betim <mbetim47@gmail.com>
2024-02-06 22:11:56 -0300

    feat(vim): add the lazyvim extra for tests with vitest

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.codeium" },
 		{ import = "lazyvim.plugins.extras.util.dot" },
 		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+		{ import = "lazyvim.plugins.extras.test.core" },
 
 		-- import/override with your plugins
 		{ import = "plugins" },

62327951d1834e0a80a0e50c682a01e24589946b
Matheus Betim <mbetim47@gmail.com>
2024-03-07 23:04:04 -0300

    feat(vim): use dynamic home path for node binaries

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,9 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- TODO: try to make this use the $HOME path and fetch the latest lts version automatically
-local node_bin = "/Users/matheus.betim/.nvm/versions/node/v20.10.0/bin"
-vim.g.node_host_prog = "/Users/matheus.betim/.nvm/versions/node/v20.10.0/bin/neovim-node-host"
+local home_dir = vim.fn.expand("~")
+local node_bin = home_dir .. "/.nvm/versions/node/v20.10.0/bin"
+vim.g.node_host_prog = home_dir .. "/.nvm/versions/node/v20.10.0/bin/neovim-node-host"
 vim.cmd("let $PATH = '" .. node_bin .. ":' . $PATH")
 
 vim.opt.clipboard = nil

1fb8a0078eb57a689f0a367f4ac14a4789985eac
Matheus Betim <mbetim47@gmail.com>
2024-03-24 15:26:06 -0300

    refactor(vim): update the `vim-tmux-navigator` plugin and move the keymaps to the plugin file

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -25,9 +25,3 @@ vim.keymap.set(
 	require("telescope.builtin").resume,
 	{ desc = "Resume", silent = true, noremap = true }
 )
-
--- Use TmuxNaivagetor to navigate between tmux panes
-vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
-vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
-vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
-vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,4 +1,16 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  lazy = false,
+	"christoomey/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+	},
+	keys = {
+		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Move to the left tmux pane" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Move to the bottom tmux pane" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Move to the top tmux pane" },
+		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Move to the right tmux panw" },
+	},
 }

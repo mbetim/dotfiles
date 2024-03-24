1492b39a9eda91fbf8e4557d9827d4a4929ce2ed
Matheus Betim <mbetim47@gmail.com>
2024-03-24 15:27:57 -0300

    feat(vim): install and set up `git-worktree` with telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,11 +3,23 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"ThePrimeagen/harpoon",
+		"ThePrimeagen/git-worktree.nvim",
 		config = function()
 			require("telescope").load_extension("harpoon")
+			require("telescope").load_extension("git_worktree")
 		end,
 	},
 	keys = {
 		{ "<leader>hh", "<cmd>Telescope harpoon marks<cr>", desc = "Show harpoon marks" },
+		{
+			"<leader>gta",
+			"<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
+			desc = "Add git worktree",
+		},
+		{
+			"<leader>gts",
+			"<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>",
+			desc = "Show git worktrees",
+		},
 	},
 }

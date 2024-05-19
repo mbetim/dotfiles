50c7597e2047e5f63b6567e0d88d812c76097d6a
Matheus Betim <mbetim47@gmail.com>
2024-05-19 20:30:24 -0300

    feat(vim): add configs for testing/debugging with jest

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -2,8 +2,28 @@ return {
 	"nvim-neotest/neotest",
 	dependencies = {
 		"marilari88/neotest-vitest",
+		"nvim-neotest/neotest-jest",
 	},
 	opts = function(_, opts)
 		table.insert(opts.adapters, require("neotest-vitest"))
+
+		table.insert(
+			opts.adapters,
+			require("neotest-jest")({
+				-- jestCommand = "npm test --",
+				jestCommand = require("neotest-jest.jest-util").getJestCommand(vim.fn.expand("%:p:h")),
+				jestConfigFile = "",
+				jest_test_discovery = false,
+				env = { CI = true },
+				cwd = function()
+					return vim.fn.getcwd()
+				end,
+			})
+		)
+
+		opts.discovery = {
+			enabled = false,
+		}
+		return opts
 	end,
 }

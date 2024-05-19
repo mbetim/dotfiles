154bb90c412aea7ddf3774d451c1ebc33b5bbc1b
Matheus Betim <mbetim47@gmail.com>
2024-05-19 20:30:52 -0300

    fix(vim): explicit use the tmux navigator in normal mode
    This was conflicting with the snippets in Select mode

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -9,8 +9,8 @@ return {
 	},
 	keys = {
 		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Move to the left tmux pane" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Move to the bottom tmux pane" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Move to the top tmux pane" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Move to the right tmux panw" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", mode = { "n" }, desc = "Move to the bottom tmux pane" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", mode = { "n" }, desc = "Move to the top tmux pane" },
+		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", mode = { "n" }, desc = "Move to the right tmux panw" },
 	},
 }

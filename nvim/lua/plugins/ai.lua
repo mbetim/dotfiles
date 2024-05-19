a4d23107ea23058823a7170003ec45087a844e9b
Matheus Betim <mbetim47@gmail.com>
2024-05-19 20:27:16 -0300

    refactor(vim): use copilot as the AI assistant

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,10 +1,11 @@
 return {
-	"nvim-cmp",
-	opts = function(_, opts)
-		table.insert(opts.sources, 1, {
-			name = "codeium",
-			group_index = 1,
-			priority = 1,
-		})
-	end,
+	-- "nvim-cmp",
+	-- opts = function(_, opts)
+	-- 	table.insert(opts.sources, 1, {
+	-- 		name = "codeium",
+	-- 		group_index = 1,
+	-- 		priority = 1,
+	-- 	})
+	-- end,
+	"github/copilot.vim",
 }

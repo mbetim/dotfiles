747fc715cdda2dee523cb0f8dae8425b18f971da
Matheus Betim <mbetim47@gmail.com>
2024-06-12 08:34:03 -0300

    feat(vim): add some configs for oil.nvim

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,20 +1,11 @@
-if true then
-	return {}
-end
-
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	opts = {
-		filesystem = {
-			filtered_items = {
-				visible = true,
-				hide_dotfiles = false,
-				hide_gitignored = false,
-				hide_by_name = {
-					"node_modules",
-					".git",
-				},
-			},
+		window = {
+			position = "right",
 		},
+	},
+	keys = {
+		{ "<leader>e", false },
 	},
 }
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,9 +1,28 @@
 return {
 	"stevearc/oil.nvim",
-	config = function()
-		require("oil").setup()
-	end,
-	keys = {
-		{ "-", "<cmd>Oil<cr>", desc = "Open oil", mode = "n" },
+	opts = {
+		default_file_explorer = true,
+		delete_to_trash = true,
+		view_options = {
+			show_hidden = true,
+			is_always_hidden = function(name, _)
+				return name == ".." or name == ".git"
+			end,
+		},
+		win_options = {
+			wrap = true,
+		},
+		keymaps = {
+			["q"] = "actions.close",
+			["<leader>e"] = "actions.close",
+		},
 	},
+	keys = function()
+		local cwd = vim.uv.cwd()
+
+		return {
+			{ "-", "<cmd>Oil<cr>", desc = "Open oil", mode = "n" },
+			{ "<leader>e", "<cmd>Oil " .. cwd .. "<cr>", desc = "Open oil", mode = "n" },
+		}
+	end,
 }

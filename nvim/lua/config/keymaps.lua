0114d3f013748e250d5ca000a1def790af7a48a0
Matheus Betim <mbetim47@gmail.com>
2024-07-03 21:59:16 -0300

    refactor(vim): remove the unused `<C-p>` keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,8 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local Util = require("lazyvim.util")
-
 -- Copy from vim
 vim.keymap.set("n", "<C-y>", '"+y')
 vim.keymap.set("v", "<C-y>", '"+y')
@@ -14,9 +12,6 @@ vim.keymap.set("n", "vv", "v$", { desc = "Select to the end of the line" })
 -- Keep cursor in the middle of the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
--- Find files (telescope)
-vim.keymap.set("n", "<C-p>", Util.telescope("files"), { desc = "Find files", silent = true, noremap = true })
 
 -- Resume last telescope window
 vim.keymap.set(

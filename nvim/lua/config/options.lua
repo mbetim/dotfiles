771716d5a6581c90f936706941d1f79b1ea72d2b
Matheus Betim <mbetim47@gmail.com>
2024-09-09 15:20:55 -0300

    feat(vim): set the textwidth to 80 for markdown files

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -17,4 +17,10 @@ vim.o.termguicolors = true
 
 vim.o.conceallevel = 2
 
-vim.o.termguicolors = true
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	pattern = { "*.md" },
+	callback = function()
+		vim.opt.colorcolumn = "80"
+		vim.opt.textwidth = 80
+	end,
+})

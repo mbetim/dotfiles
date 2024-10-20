6da1372d37dd14418fe5292aec445353f2d64a15
Matheus Betim <mbetim47@gmail.com>
2024-10-20 13:38:11 -0300

    feat(vim): update the `path_display`, add a mapping to delete a buffer and update default lazyvim keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,6 +3,16 @@ return {
 	opts = {
 		defaults = {
 			layout_strategy = "vertical",
+			path_display = { "filename_first" },
+			mappings = {
+				n = {
+					["d"] = require("telescope.actions").delete_buffer,
+				},
+			},
 		},
+	},
+	keys = {
+		{ "<leader><space>", "<cmd>Telescope frecency<cr>", desc = "Find files (CWD)" },
+		{ "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
 	},
 }

b7ecd1fa36df1e7026788e0ae1de1f7e7be1b8e8
Matheus Betim <mbetim47@gmail.com>
2024-10-20 13:37:20 -0300

    feat(vim): add the `telescope-frecency` plugin

---
+++ nvim/lua/plugins/telescope-frecency.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-telescope/telescope-frecency.nvim",
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				frecency = {
+					show_scores = true,
+					show_filter_column = false,
+					default_workspace = "CWD",
+					path_display = { "filename_first" },
+					db_safe_mode = false,
+				},
+			},
+		})
+		require("telescope").load_extension("frecency")
+	end,
+}

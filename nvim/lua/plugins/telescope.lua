f6792edd542327fcef7d868d4840cd302ecbb7a6
Matheus Betim <mbetim47@gmail.com>
2024-12-27 12:01:08 -0300

    feat(vim): force all telescope themes to be ivy

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	opts = {
-		defaults = {
+		defaults = vim.tbl_deep_extend("force", {
 			layout_strategy = "vertical",
 			path_display = { "filename_first" },
 			mappings = {
@@ -9,10 +9,10 @@ return {
 					["d"] = require("telescope.actions").delete_buffer,
 				},
 			},
-		},
+		}, require("telescope.themes").get_ivy()),
 	},
 	keys = {
-		{ "<leader><space>", "<cmd>Telescope frecency<cr>", desc = "Find files (CWD)" },
+		{ "<leader><space>", "<cmd>Telescope frecency theme=ivy<cr>", desc = "Find files (CWD)" },
 		{ "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
 	},
 }

0627a67bda66fcbc4c7c84341dae9a743ade24a8
Matheus Betim <mbetim47@gmail.com>
2025-02-17 20:46:17 -0300

    feat(vim): add types and disable the `auto_brackets` option in blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,6 +1,13 @@
 return {
 	"saghen/blink.cmp",
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
 	opts = {
+		completion = {
+			accept = {
+				auto_brackets = { enabled = false },
+			},
+		},
 		keymap = {
 			preset = "default",
 			["<Up>"] = { "select_prev", "fallback" },

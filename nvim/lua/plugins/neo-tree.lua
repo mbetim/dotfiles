391819df8c2bb27e550b00a69db18ae76148482d
Matheus Betim <mbetim47@gmail.com>
2025-02-17 20:46:44 -0300

    fix(vim): properly disable neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,4 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	opts = {
-		window = {
-			position = "right",
-		},
-	},
-	keys = {
-		{ "<leader>e", false },
-		{ "<leader>E", false },
-	},
+	enabled = false,
 }

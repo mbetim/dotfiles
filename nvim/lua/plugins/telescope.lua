192b551c8d370c996b24b50f87d369a31279aad2
Matheus Betim <mbetim47@gmail.com>
2025-02-18 18:02:02 -0300

    fix(vim): remove duplicated keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -12,7 +12,7 @@ return {
 		}, require("telescope.themes").get_ivy()),
 	},
 	keys = {
-		{ "<leader><space>", "<cmd>Telescope frecency theme=ivy<cr>", desc = "Find files (CWD)" },
-		{ "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
+		-- { "<leader><space>", "<cmd>Telescope frecency theme=ivy<cr>", desc = "Find files (CWD)" },
+		-- { "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
 	},
 }

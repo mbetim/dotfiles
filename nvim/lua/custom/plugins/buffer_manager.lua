e46e817ea70042ef9fa814f936c5d79c0a806836
Matheus Betim <mbetim47@gmail.com>
2025-07-02 10:53:12 -0300

    feat(vim): increase buffer manager width and add keymaps for buffer navigation with Alt+number

--- nvim/lua/custom/plugins/buffer_manager.lua
+++ nvim/lua/custom/plugins/buffer_manager.lua
@@ -1,29 +1,43 @@
 return {
   'j-morano/buffer_manager.nvim',
   opts = {
-    width = 0.5,
+    width = 0.75,
   },
-  keys = {
-    {
-      '<leader>,',
-      function()
-        require('buffer_manager.ui').toggle_quick_menu()
-      end,
-      desc = 'Buffer Manager',
-    },
-    {
-      'L',
-      function()
-        require('buffer_manager.ui').nav_next()
-      end,
-      desc = 'Next buffer',
-    },
-    {
-      'H',
-      function()
-        require('buffer_manager.ui').nav_prev()
-      end,
-      desc = 'Next buffer',
-    },
-  },
+  keys = function()
+    local keymaps = {
+      {
+        '<leader>,',
+        function()
+          require('buffer_manager.ui').toggle_quick_menu()
+        end,
+        desc = 'Buffer Manager',
+      },
+      {
+        'L',
+        function()
+          require('buffer_manager.ui').nav_next()
+        end,
+        desc = 'Next buffer',
+      },
+      {
+        'H',
+        function()
+          require('buffer_manager.ui').nav_prev()
+        end,
+        desc = 'Next buffer',
+      },
+    }
+
+    for i = 1, 9 do
+      table.insert(keymaps, {
+        string.format('<M-%d>', i),
+        function()
+          require('buffer_manager.ui').nav_file(i)
+        end,
+        desc = 'Move to buffer ' .. i,
+      })
+    end
+
+    return keymaps
+  end,
 }

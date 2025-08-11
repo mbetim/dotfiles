742aec257b3d98e56141812b693c50d6e3f09312
Matheus Betim <mbetim47@gmail.com>
2025-08-11 11:55:07 -0300

    feat(vim): add overseer to run jobs

---
+++ nvim/lua/custom/plugins/overseer.lua
@@ -0,0 +1,16 @@
+return {
+  'stevearc/overseer.nvim',
+  opts = {},
+  keys = {
+    {
+      '<leader>or',
+      '<cmd>OverseerRun<cr>',
+      desc = 'Overseer Run',
+    },
+    {
+      '<leader>oo',
+      '<cmd>OverseerToggle<cr>',
+      desc = 'Overseer Toggle',
+    },
+  },
+}

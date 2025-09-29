b4c4ed0dc4badf4e8f7fbddfe7cedf1901d4eecc
Matheus Betim <mbetim47@gmail.com>
2025-09-29 15:00:59 -0300

    feat(vim): add the chainsaw plugin to quickly write logs

---
+++ nvim/lua/custom/plugins/logs.lua
@@ -0,0 +1,21 @@
+return {
+  'chrisgrieser/nvim-chainsaw',
+  event = 'VeryLazy',
+  opts = {}, -- required even if left empty
+  keys = {
+    {
+      '<leader>ll',
+      function()
+        require('chainsaw').variableLog()
+      end,
+      desc = 'Log var',
+    },
+    {
+      '<leader>lr',
+      function()
+        require('chainsaw').removeLogs()
+      end,
+      desc = 'Remove all logs',
+    },
+  },
+}

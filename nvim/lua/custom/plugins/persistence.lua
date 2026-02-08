01170eabc2aa854dbd817c51d8a982046e4a8589
Matheus Betim <mbetim47@gmail.com>
2026-02-08 15:37:30 -0300

    feat(vim): add keymap to restore a persistence session

--- nvim/lua/custom/plugins/persistence.lua
+++ nvim/lua/custom/plugins/persistence.lua
@@ -2,4 +2,13 @@ return {
   'folke/persistence.nvim',
   event = 'BufReadPre', -- this will only start session saving when an actual file was opened
   opts = {},
+  keys = {
+    {
+      '<leader>qs',
+      function()
+        require('persistence').load()
+      end,
+      desc = 'Restore Session',
+    },
+  },
 }

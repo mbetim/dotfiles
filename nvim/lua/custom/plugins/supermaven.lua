e74c155f83947646ea05a0449ff160799efb6a19
Matheus Betim <mbetim47@gmail.com>
2026-02-27 14:27:19 -0300

    feat(vim): enable supermaven again and remap accept word and clear suggestion commands

--- nvim/lua/custom/plugins/supermaven.lua
+++ nvim/lua/custom/plugins/supermaven.lua
@@ -1,21 +1,14 @@
-if true then
-  return {}
-end
-
 vim.keymap.set('n', '<leader>ts', ':SupermavenToggle<cr>', { desc = 'Supermaven Toggle' })
 
 return {
   'supermaven-inc/supermaven-nvim',
-  dependencies = {
-    'huijiro/blink-cmp-supermaven',
-  },
   opts = {
     -- disable_inline_completion = true, -- disables inline completion for use with cmp
     -- disable_keymaps = true, -- disables built in keymaps for more manual control
     keymaps = {
       accept_suggestion = '<Tab>',
-      clear_suggestion = '<C-]>',
-      accept_word = '<C-e>',
+      clear_suggestion = '<c-]>',
+      accept_word = '<C-b>',
     },
 
     ignore_filetypes = {
